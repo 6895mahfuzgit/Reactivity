0280d37bb59c7d57a4df350a77c6fbceb4b3359e
mahfuzroyex123 <mahfuzroyex123@gmail.com>
2020-04-19 02:39:41 +0600

    Added Context To Data Project  and completed first dumy migration of DB.

---
+++ Reactivity.Infra.Data/Context/ReactivlyDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Reactivity.Domain.Models;
+
+namespace Reactivity.Infra.Data.Context
+{
+    public class ReactivlyDBContext : DbContext
+    {
+        public ReactivlyDBContext(DbContextOptions options) : base(options)
+        {
+        }   
+
+        public DbSet<Value>  Values{ get; set; }
+    }
+
+    public class ReactivlyDBContextFactory : IDesignTimeDbContextFactory<ReactivlyDBContext>
+    {
+        public ReactivlyDBContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ReactivlyDBContext>();
+            optionsBuilder.UseSqlServer("Server=LAPTOP-TT5P3OP6\\SQLEXPRESS;Database=ReactivlyBD;Trusted_Connection=True;MultipleActiveResultSets=True;");
+            return new ReactivlyDBContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Reactivity.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Reactivity.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reactivity.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<ReactivlyDBContext>();
+        }
+    }
+}
